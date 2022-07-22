badf4ef9e86b142901ae31266f8e1d4375dd8779
Matthew Gonzales <mattgonzales3@yahoo.com>
2022-07-22 10:30:59 -0500

    added webpack plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,57 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      // plugin injects bundles and generates html
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }) ,
+
+            // Injects our custom service worker
+            new InjectManifest({
+              swSrc: './src-sw.js',
+              swDest: 'src-sw.js',
+            }) ,
+
+            new WebpackPwaManifest({
+              fingerprints: false,
+              inject: true,
+              name: 'JATE',
+              short_name: 'JATE',
+              description: 'Just Another Text Editor, with PWA',
+              background_color: '#225ca3',
+              theme_color: '#225ca3',
+              start_url: '/',
+              publicPath: '/',
+              icons: [
+                {
+                  src: path.resolve('src/images/logo.png'),
+                  sizes: [96, 128, 192, 256, 384, 512],
+                  destination: path.join('assets', 'icons'),
+                },
+              ],
+            }),
       
     ],
 
     module: {
       rules: [
-        
+        //css loaders
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
